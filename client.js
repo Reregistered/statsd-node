d11302e1dff855ee1952f8ca45920bc4c652c571
Dov Amihod <dov@getTeamUp.com>
2012-07-17 00:33:44 -0400

    first version

---
+++ client.js
@@ -0,0 +1,53 @@
+/*
+ */
+
+var dgram = require('dgram');
+
+var client = exports.client = function(params){
+
+  params = params || {};
+
+  this.port = params.port || '';
+  this.host = params.host || 'localhost';
+
+  ////////////////////////////////////////
+  // create a new class with a UDP connection
+  this.client = dgram.createSocket("udp4");
+
+  return this;
+
+};
+
+
+client.prototype.count = function(name, count, interval){
+
+  var str = name + ':' + count + '|c' + (interval ? '|@' + interval : '');
+  var buf = new Buffer(str);
+
+  this.send(buf);
+
+};
+
+client.prototype.timing = function(name, timing){
+
+  var str = name + ':' + timing + '|ms';
+  var buf = new Buffer(str);
+
+  this.send(buf);
+
+};
+
+client.prototype.gauge = function(name,val){
+
+  var str = name + ':' + val + '|g';
+  var buf = new Buffer(str);
+
+  this.send(buf);
+
+};
+
+client.prototype.send = function(buf){
+
+  this.client.send(buf, 0, buf.length, this.port, this.host);
+
+};
